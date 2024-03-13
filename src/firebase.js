cd00440255922e3a645068151a65e89bb40eb36b
DomagojKasalo <44065517+DomagojKasalo@users.noreply.github.com>
2024-03-13 16:27:13 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtjo6GNP01AxprloZddyuYzGIXLeo4g2I",
-  authDomain: "classicl.firebaseapp.com",
-  projectId: "classicl",
-  storageBucket: "classicl.appspot.com",
-  messagingSenderId: "928627670801",
-  appId: "1:928627670801:web:be9aa527577511e8fe96b0",
-  measurementId: "G-15CKGTS8R9"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "appspot.com",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
